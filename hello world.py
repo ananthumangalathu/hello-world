d3481ddfe18563f37f4f311b046e133f1f4f2b50
Ananthu Mangalathu <ananthumangalathu@gmail.com>
2021-05-04 14:20:39 +0530

    compleated

--- hello world.py
+++ hello world.py
@@ -1,7 +1,7 @@
 for i in range(1,8):
     for j in range(1,200):
         if(i==1):
-            if(j in [1,13,33,49,86,92,98,134] or j in range(17,30,2) or j in range(64,77,2) or j in range (102,115,2) or j in range (118,131,2) or j in range (150,163,2)):
+            if(j in [1,13,33,49,86,92,98,134] or j in range(17,30,2) or j in range(66,75,2) or j in range (104,113,2) or j in range (118,129,2) or j in range (150,161,2)):
                 print("C", end="")
             else:
                 print(" " ,end="")
@@ -16,7 +16,7 @@ for i in range(1,8):
             else:
                 print(" ",end="")
         if(i==4):
-            if(j in [33,49,64,76,86,92,98,102,114,134,150,162] or j in range (1,14,2) or j in range (17,30,2) or j in range (118,131,2)):
+            if(j in [33,49,64,76,86,92,98,102,114,134,150,162] or j in range (1,14,2) or j in range (17,30,2) or j in range (118,129,2)):
                 print("C", end="")
             else:
                 print(" ",end="")
@@ -31,7 +31,7 @@ for i in range(1,8):
             else:
                 print(" ",end="")
         if(i==7):
-            if(j in [1,13,118,130] or j in range(17,30,2) or j in range (33,46,2) or j in range (49,61,2) or j in range (64,77,2) or j in range (86,99,2) or j in range (102,115,2) or j in range (134,147,2) or j in range (150,163,2)):
+            if(j in [1,13,88,90,94,96,118,130] or j in range(17,30,2) or j in range (33,46,2) or j in range (49,61,2) or j in range (66,75,2) or j in range (104,113,2) or j in range (134,147,2) or j in range (150,161,2)):
                 print("C",end="")
             else:
                 print(" ",end="")
