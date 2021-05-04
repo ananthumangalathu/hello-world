4eac27f88482ca5c9f0a197684e5ed61413687d1
Ananthu Mangalathu <ananthumangalathu@gmail.com>
2021-05-04 14:12:58 +0530

    position and size adjusted

--- hello world.py
+++ hello world.py
@@ -1,37 +1,37 @@
 for i in range(1,8):
     for j in range(1,200):
         if(i==1):
-            if(j in [1,11,30,44,83,89,95,129] or j in range(15,26,2) or j in range(58,69,2) or j in range (99,110,2) or j in range (114,125,2) or j in range (143,154,2)):
+            if(j in [1,13,33,49,86,92,98,134] or j in range(17,30,2) or j in range(64,77,2) or j in range (102,115,2) or j in range (118,131,2) or j in range (150,163,2)):
                 print("C", end="")
             else:
                 print(" " ,end="")
         if(i==2):
-            if(j in [1,11,15,30,44,58,68,83,89,95,99,109,114,124,129,143,153]):
+            if(j in [1,13,17,33,49,64,76,86,92,98,102,114,118,130,134,150,162]):
                 print("C",end="")
             else:
                 print(" ",end="")
         if(i==3):
-            if(j in [1,11,15,30,44,58,68,83,89,95,99,109,114,124,129,143,153]):
+            if(j in [1,13,17,33,49,64,76,86,92,98,102,114,118,130,134,150,162]):
                 print("C",end="")
             else:
                 print(" ",end="")
         if(i==4):
-            if(j in [30,44,58,68,83,89,95,99,109,129,143,153] or j in range (1,12,2) or j in range (15,26,2) or j in range (114,125,2)):
+            if(j in [33,49,64,76,86,92,98,102,114,134,150,162] or j in range (1,14,2) or j in range (17,30,2) or j in range (118,131,2)):
                 print("C", end="")
             else:
                 print(" ",end="")
         if(i==5):
-            if(j in [1,11,15,30,44,58,68,83,89,95,99,109,114,124,129,143,153]):
+            if(j in [1,13,17,33,49,64,76,86,92,98,102,114,118,130,134,150,162]):
                 print("C",end="")
             else:
                 print(" ",end="")
         if(i==6):
-            if(j in [1,11,15,30,44,58,68,83,89,95,99,109,114,124,129,143,153]):
+            if(j in [1,13,17,33,49,64,76,86,92,98,102,114,118,130,134,150,162]):
                 print("C",end="")
             else:
                 print(" ",end="")
         if(i==7):
-            if(j in [1,11,114,124] or j in range(15,26,2) or j in range (30,41,2) or j in range (44,55,2) or j in range (58,69,2) or j in range (83,96,2) or j in range (99,110,2) or j in range (129,140,2) or j in range (143,154,2)):
+            if(j in [1,13,118,130] or j in range(17,30,2) or j in range (33,46,2) or j in range (49,61,2) or j in range (64,77,2) or j in range (86,99,2) or j in range (102,115,2) or j in range (134,147,2) or j in range (150,163,2)):
                 print("C",end="")
             else:
                 print(" ",end="")
